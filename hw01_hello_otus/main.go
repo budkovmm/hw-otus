99bfaad95d5bc1ae17183fc67a5499c3b2905e19
Mikhail Budkov <mbudkov@ozon.ru>
2022-06-06 19:44:37 +0200

    delete wrapper function for reverse and delete test

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -5,12 +5,7 @@ import (
 	"golang.org/x/example/stringutil"
 )
 
-// ReverseString function return reversed string
-func ReverseString(s string) string {
-	return stringutil.Reverse(s)
-}
-
 func main() {
-	reversedString := ReverseString("Hello, OTUS!")
+	reversedString := stringutil.Reverse("Hello, OTUS!")
 	fmt.Println(reversedString)
 }
